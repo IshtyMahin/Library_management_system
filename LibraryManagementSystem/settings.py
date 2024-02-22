a0ee6494642cb6245ec320143ec3ec1d1cd16ffc
IshtyMahin <ishty119@gmail.com>
2024-02-22 22:24:08 +0600

    csrf trusted origins add

--- LibraryManagementSystem/settings.py
+++ LibraryManagementSystem/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
-CSRF_TRUSTED_ORIGINS = ['https://library-management-system-1-3c6r.onrender.com','https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://library-management-system-x5ky.onrender.com','https://*.127.0.0.1']
 
 # Application definition
 
